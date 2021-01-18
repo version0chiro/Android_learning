cf78117cd80b13b7fed9b6950176872e96ee3d01
Chiro <sachinsinghroxx@gmail.com>
2021-01-18 14:09:03 +0530

    made the main game

--- personal_projects/BrainTrainer/app/src/main/java/com/example/braintrainer/MainActivity.java
+++ personal_projects/BrainTrainer/app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -8,16 +8,61 @@ import android.view.View;
 import android.widget.Button;
 import androidx.gridlayout.widget.GridLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     boolean GameRunning=false;
     TextView timer;
+    TextView sum;
+    TextView option1;
+    TextView option2;
+    int totalAsked=0;
+    TextView option3;
+    TextView option4;
+    int userScore;
+    TextView score;
+    int rightAnswer;
+    Random rand;
+    TextView answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rand = new Random();
+        option1 = (TextView)findViewById(R.id.option1);
+        option2 = (TextView)findViewById(R.id.option2);
+        option3 = (TextView)findViewById(R.id.option3);
+        option4 = (TextView)findViewById(R.id.option4);
+
+
+    }
+
+    public void generateSum(){
+        rightAnswer = rand.nextInt(100);
+        int firstDigit = rand.nextInt(rightAnswer);
+        int secondDigit = rightAnswer - firstDigit;
+        String sumString = Integer.toString(firstDigit) + "+" + Integer.toString(secondDigit);
+        int rightOption = rand.nextInt(4)+1;
+        List<Integer> options = new ArrayList<Integer>();
+        for (int i =1;i<5;i++){
+            if(i==rightOption){
+                options.add(rightAnswer);
+            }
+            else{
+
+                options.add(rand.nextInt(100));
+            }
+        }
+        option1.setText(Integer.toString(options.get(0)));
+        option2.setText(Integer.toString(options.get(1)));
+        option3.setText(Integer.toString(options.get(2)));
+        option4.setText(Integer.toString(options.get(3)));
+        sum.setText(sumString);
     }
 
     public void switchView(){
@@ -28,11 +73,11 @@ public class MainActivity extends AppCompatActivity {
         gridLayout.setVisibility(View.VISIBLE);
         timer = (TextView) findViewById(R.id.timer);
         timer.setVisibility(View.VISIBLE);
-        TextView score = (TextView) findViewById(R.id.score);
+        score = (TextView) findViewById(R.id.score);
         score.setVisibility(View.VISIBLE);
-        TextView sum = (TextView) findViewById(R.id.sum);
+        sum = (TextView) findViewById(R.id.sum);
         sum.setVisibility(View.VISIBLE);
-        TextView answer = (TextView) findViewById(R.id.answer);
+        answer = (TextView) findViewById(R.id.answer);
         answer.setVisibility(View.VISIBLE);
 
 
@@ -41,15 +86,25 @@ public class MainActivity extends AppCompatActivity {
             Button startButton = (Button) findViewById(R.id.Start);            startButton.setVisibility(View.VISIBLE);
             GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
             gridLayout.setVisibility(View.GONE);
-            TextView timer = (TextView) findViewById(R.id.timer);
+            timer = (TextView) findViewById(R.id.timer);
             timer.setVisibility(View.GONE);
-            TextView score = (TextView) findViewById(R.id.score);
+            score = (TextView) findViewById(R.id.score);
             score.setVisibility(View.GONE);
-            TextView sum = (TextView) findViewById(R.id.sum);
+            sum = (TextView) findViewById(R.id.sum);
             sum.setVisibility(View.GONE);
-            TextView answer = (TextView) findViewById(R.id.answer);
+            answer = (TextView) findViewById(R.id.answer);
             answer.setVisibility(View.GONE);
         }
+    }
+
+    public  void updateScore(boolean status){
+        totalAsked++;
+        if (status==true){
+            userScore++;
+        }
+        String scoreString = Integer.toString(userScore) +"/"+Integer.toString(totalAsked);
+        score.setText(scoreString);
+
     }
     public void startGame(View view) {
         GameRunning = true;
@@ -59,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTick(long millisUntilFinished) {
-                timer.setText((int) TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished));
+                timer.setText(Integer.toString((int) TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished)));
             }
 
             @Override
@@ -68,6 +123,28 @@ public class MainActivity extends AppCompatActivity {
                 switchView();
             }
         }.start();
+
+        generateSum();
+
+
+
+    }
+
+    public void submitAnswer(View view) {
+        TextView submittedAnswerView = (TextView) view;
+        Integer submittedAnswer = Integer.parseInt((String) submittedAnswerView.getText());
+        updateScore(submittedAnswer==rightAnswer);
+        if(submittedAnswer==rightAnswer){
+            Toast.makeText(this, "Right Answer", Toast.LENGTH_SHORT).show();
+            System.out.println("Right Answer");
+            answer.setText("Right Answer");
+        }
+        else {
+            Toast.makeText(this, "Wrong Answer", Toast.LENGTH_SHORT).show();
+            System.out.println("Wrong Answer");
+            answer.setText("Wrong Answer");
+        }
+        generateSum();
 
     }
 }
