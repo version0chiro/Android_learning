551f32c648c1bf8f1485f17d8653d70a5d1053ee
Chiro <sachinsinghroxx@gmail.com>
2021-01-19 15:14:44 +0530

    made anime guess character game

--- personal_projects/DownloadImages/app/src/main/java/com/example/downloadimages/MainActivity.java
+++ personal_projects/DownloadImages/app/src/main/java/com/example/downloadimages/MainActivity.java
@@ -2,13 +2,81 @@ package com.example.downloadimages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView downloadImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        downloadImg = (ImageView) findViewById(R.id.image);
+
+    }
+
+    public class ImageDownloader extends AsyncTask<String,Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+
+
+        }
+    }
+
+    public void changeImage(View view) {
+        //https://i.pinimg.com/originals/e1/1f/06/e11f0644f5cac8de0c875cf246dc35dc.jpg
+        ImageDownloader task = new ImageDownloader();
+
+        Bitmap myImage;
+        try {
+            myImage = task.execute("https://i.pinimg.com/originals/e1/1f/06/e11f0644f5cac8de0c875cf246dc35dc.jpg").get();
+            downloadImg.setImageBitmap(myImage);
+
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.i("Interaction","Button Tapped");
     }
 }
---
+++ personal_projects/GuessTheAnimeCharacter/app/src/main/java/com/example/guesstheanimecharacter/MainActivity.java
@@ -0,0 +1,204 @@
+package com.example.guesstheanimecharacter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    ImageView img;
+    String correctAns;
+    List<String> urlStrings = new ArrayList<>();
+    List<String> nameStrings = new ArrayList<>();
+
+    public class DownloadHtml extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = ""; //output
+
+            URL url; //link
+
+            HttpURLConnection urlConnection = null; //browser
+            try{
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while (data !=-1){
+
+                    char current = (char)data ;
+
+                    result +=current;
+                    data = reader.read();
+                }
+
+                return result;
+            }
+            catch (Exception e){
+                e.printStackTrace();
+                return "FAILED";
+            }
+//            Log.i("URL",strings[0]);
+
+
+        }
+    }
+
+    public class ImageDownloader extends AsyncTask<String,Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try{
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpsURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.option1);
+        button2 = (Button) findViewById(R.id.option2);
+        button3 = (Button) findViewById(R.id.option3);
+        button4 = (Button) findViewById(R.id.option4);
+        img = (ImageView) findViewById(R.id.imageView);
+        DownloadHtml htmlTask = new DownloadHtml();
+        String results = null;
+        try{
+            results = htmlTask.execute("https://www.fandomspot.com/strongest-anime-characters/").get();
+        }catch (Exception  e){
+            e.printStackTrace();
+        }
+
+        Pattern p = Pattern.compile("img src=(.*?)>");
+
+        Matcher m = p.matcher(results);
+
+        while (m.find()){
+
+            String[] temp = m.group(1).split(" alt=");
+//            System.out.println(Arrays.toString(temp));
+            try {
+                if(temp.length>0){
+                    urlStrings.add(temp[0]);
+                    nameStrings.add(temp[1]);
+                }
+
+            }
+            catch (Exception e){
+                System.out.println(Arrays.toString(temp));
+            }
+        }
+        nameStrings = nameStrings.subList(0,50);
+        urlStrings=urlStrings.subList(1,51);
+        updateImage();
+    }
+
+    public void updateImage(){
+        Random random = new Random();
+        int diceRoll = random.nextInt(50);
+        int rightOption = random.nextInt(4)+1;
+        String url = urlStrings.get(diceRoll);
+        correctAns = nameStrings.get(diceRoll);
+        url = url.substring(1,url.length());
+        Bitmap myImage;
+        List<String> options = new ArrayList<>();
+        for(int i =0;i<4;i++){
+            int randomOption= random.nextInt(50);
+            if(i==rightOption){
+                options.add(correctAns);
+            }
+            else if(randomOption!= diceRoll){
+                options.add(nameStrings.get(randomOption));
+            }
+            else{
+                i--;
+            }
+
+        }
+
+        button1.setText((options.get(0)));
+        button2.setText((options.get(1)));
+        button3.setText((options.get(2)));
+        button4.setText((options.get(3)));
+        ImageDownloader task = new ImageDownloader();
+        try{
+            myImage = task.execute(url).get();
+            img.setImageBitmap(myImage);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void submitAnswer(View view) {
+//        ImageDownloader task = new ImageDownloader();
+
+
+        try{
+            Button pressedButton = (Button) view;
+            String submittedAnswer = (String) pressedButton.getText();
+            if(submittedAnswer==correctAns){
+                Toast.makeText(this, "Right Answer", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(this, "Wrong Answer, Correct answer is "+ correctAns, Toast.LENGTH_LONG).show();
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
